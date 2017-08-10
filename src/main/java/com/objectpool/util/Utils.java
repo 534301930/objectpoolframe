1ec9aab766bc15703c85ceb5bcd5caee7ff02f59
OXunLiO <18366137703@163.com>
2017-08-10 15:37:23 +0800

    utils

--- src/main/java/com/objectpool/util/Utils.java
+++ src/main/java/com/objectpool/util/Utils.java
@@ -7,4 +7,14 @@ package com.objectpool.util;
 public class Utils {
 
 	public static final String lineSeparator = System.getProperty("line.separator", "\n");
+	
+	public static boolean equals(String s1, String s2) {
+		if (s1 == null && s2 == null) {
+			return true;
+		} else if (s1 != null && s2 != null) {
+			return s1.equals(s2);
+		} else {
+			return false;
+		}
+	}
 }
